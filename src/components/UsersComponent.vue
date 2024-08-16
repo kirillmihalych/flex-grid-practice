30518d17269b30b3762d61441c5dc6f9693cecea
kirillmihalych <tiltlessman@gmail.com>
2024-08-16 18:18:34 +0300

    playin with git again

--- src/components/UsersComponent.vue
+++ src/components/UsersComponent.vue
@@ -7,11 +7,6 @@ import { ref, onMounted } from "vue";
 
 const users = ref([]);
 const getUsers = async () => {};
-
-onMounted(() => {});
-// Как сделать?
-// [ ] Начинаем с мобильной версии, но сразу делаем и десктоп
-// [ ] Начинаем с семантической вёрстки, пишу так, чтобы теги были ясны для меня
 </script>
 
 <style lang="scss" scoped></style>
